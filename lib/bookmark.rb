cbf675c0251b6e014fa9322e2448c65e96ef0832
Nahayo Nowomuhangi <christine_nn@live.co.uk>
2019-03-03 19:14:15 +0000

    interacting with PostgreSQL from Ruby

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,10 @@
+require 'pg'
+
 class Bookmark
   def self.all
     # all is a class method
-    [
-      'https://rubygems.org',
-      'https://medium.com',
-      'https://ruby-doc.org'
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks;')
+    result.map { |bookmark| bookmark[ 'url'] }
   end
 end
